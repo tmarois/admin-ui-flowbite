5146ef408c046b1fd18973c6e08a032ee84fb715
Timothy Marois <timothymarois@gmail.com>
2023-02-28 23:36:56 -0500

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { createResolver } from '@nuxt/kit'
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
+    ssr: false,
     runtimeConfig: {
         public: {
             BASE_URL: process.env.BASE_URL,
