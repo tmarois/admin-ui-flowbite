ebc2551b56361cf4000efe37c54fbc5822c5256f
Timothy Marois <timothymarois@gmail.com>
2023-03-01 16:15:01 -0500

    gitignore and dropdown fixes

--- components/App/Nav.vue
+++ components/App/Nav.vue
@@ -59,9 +59,9 @@
 					<span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">timothymarois@gmail.com</span>
 				</div>
 				<ul class="py-2" aria-labelledby="user-menu-button">
-					<li>
+					<!-- <li>
 						<NuxtLink href="/account/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">My Account</NuxtLink>
-					</li>
+					</li> -->
 					<li>
 						<div @click="logout" class="hover:cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</div>
 					</li>
@@ -91,5 +91,9 @@
 
 
 <script setup>
+import { initDropdowns } from 'flowbite'
 const { logout } = useAuth()
+onMounted(() => {
+    initDropdowns();
+});
 </script>
