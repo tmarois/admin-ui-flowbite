0b2f4e9c1e8197f10fb9a5f3b8a46fb0f227519a
Timothy Marois <timothymarois@gmail.com>
2023-03-08 19:38:24 -0500

    Added tooltip component

---
+++ components/Element/Tooltip.vue
@@ -0,0 +1,26 @@
+<template>
+    <div 
+        :id="`${id}`" 
+        role="tooltip" 
+        class="absolute z-10 invisible inline-block px-3 py-2 text-xs font-medium text-white bg-gray-900 rounded shadow-sm opacity-0 tooltip transition-opacity duration-300 max-w-[250px]"
+    >
+        <slot />
+        <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+</template>
+
+<script setup>
+import { initTooltips } from 'flowbite'
+const props = defineProps({
+   id: {
+      type: String,
+      default: null
+   },
+});
+
+onMounted(() => {
+    nextTick(() => {
+        initTooltips()
+    })
+});
+</script>
--- components/Form/Label.vue
+++ components/Form/Label.vue
@@ -2,6 +2,7 @@
     <label 
         :for="props.id"
         :class="labelClasses"
+        class="flex items-center"
     >
         <span v-if="slots?.icon" class="mr-1">
             <slot name="icon" />
@@ -10,6 +11,10 @@
             <span v-if="props.title">{{ props.title }}</span>
         </slot>
         <span v-if="props.required" class="text-red-500">*</span>
+        <span v-if="props.tooltip" :data-tooltip-target="`tooltip-${props.id}`" data-tooltip-placement="top" class="text-blue-600 cursor-pointer ml-1">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" /></svg>
+        </span>
+        <ElementTooltip v-if="props.tooltip" :id="`tooltip-${props.id}`" >{{ tooltip }}</ElementTooltip>
     </label>
 </template>
 
@@ -33,6 +38,10 @@ const props = defineProps({
         default: null
     },
     id: {
+        type: String,
+        default: null
+    },
+    tooltip: {
         type: String,
         default: null
     },
