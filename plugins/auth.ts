1957d5595f79e938d1125db7515804a1457a5d8e
Timothy Marois <timothymarois@gmail.com>
2023-03-28 10:40:09 -0400

    Update auth/user

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,7 +1,7 @@
 import { useUser, fetchCurrentUser } from "../composables/useAuth";
 
 export default defineNuxtPlugin(async () => {
-	// const user = useUser();
-	// if (user.value !== undefined) return;
-	// user.value = await fetchCurrentUser();
+	const user = useUser();
+	if (user.value !== undefined) return;
+	user.value = await fetchCurrentUser();
 });
